a2421ae99e699843f1343e6932c4c446d05df2e2
Marc Salo <marc@nurelm.com>
2020-11-13 10:09:48 -0500

    Added fix for main image being exclusive. Also returning sorted cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,7 +3,7 @@ class CardsController < ApplicationController
 
   # GET /cards
   def index
-    @cards = Card.all.with_attached_image
+    @cards = Card.order(updated_at: :desc).with_attached_image
 
     render json: @cards
   end
@@ -16,17 +16,19 @@ class CardsController < ApplicationController
   # POST /cards
   def create
     cats = grab_categories(card_params[:categories])
+    main_img_check = cats.count == 1 && card_params[:main_image]
     @card = Card.new(
       name: card_params[:name],
       info: card_params[:info],
       vertical: card_params[:vertical],
       sold: card_params[:sold],
-      main_image: card_params[:main_image],
+      main_image: main_img_check,
       image: card_params[:image],
       categories: cats
     )
 
     if @card.save
+      update_categories(cats.first.id, @card.id) if main_img_check
       render json: @card, status: :created, location: @card
     else
       render json: @card.errors, status: :unprocessable_entity
@@ -41,16 +43,19 @@ class CardsController < ApplicationController
     end
 
     cats = grab_categories(card_params[:categories])
+    main_img_check = cats.count == 1 && card_params[:main_image]
     if @card.update(
       {name: card_params[:name],
       info: card_params[:info],
       vertical: card_params[:vertical],
       sold: card_params[:sold],
-      main_image: card_params[:main_image],
+      main_image: main_img_check,
       image: card_params[:image],
       categories: cats
       }.compact!
     )
+
+      update_categories(cats.first.id, @card.id) if main_img_check
       render json: @card
     else
       render json: @card.errors, status: :unprocessable_entity
@@ -59,6 +64,7 @@ class CardsController < ApplicationController
 
   # DELETE /cards/1
   def destroy
+    delete_image if @card.image.attached?
     @card.destroy
   end
 
@@ -87,7 +93,16 @@ class CardsController < ApplicationController
         cat = Category.where("lower(name) = ?", name.downcase).first
         cats << cat if cat
       end
-      puts "Categories: #{cats}"
       cats
+    end
+
+    def update_categories(id, card_id)
+      main_cards = Card.where(main_image: true)
+      main_cards.each do |m|
+        next if m.id == card_id || m.categories.count > 1 || m.categories.first.id != id
+
+        m.main_image = false
+        m.save
+      end
     end
 end
